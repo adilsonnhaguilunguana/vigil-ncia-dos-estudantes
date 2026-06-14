7942757f15e7d5c6203916a82fa96693858421a3
ADILSON JAIME <adilsonnhaguilunguana@gmail.com>
2026-06-14 16:31:43 +0200

    nada

---
+++ .history/opencv_20260613161810.py
@@ -0,0 +1,31 @@
+import cv2
+
+# ⚠️ Troca pelo IP que aparece no teu celular!
+IP_CELULAR = "10:8080"
+
+url = f"http://{IP_CELULAR}/video"
+
+camera = cv2.VideoCapture(url)
+
+# Cria a janela com nome
+cv2.namedWindow("Camera Celular", cv2.WINDOW_NORMAL)
+
+# Define o tamanho (largura x altura)
+cv2.resizeWindow("Camera Celular", 800, 600)
+
+while True:
+    ret, frame = camera.read()
+
+    if not ret:
+        break
+
+    cv2.putText(frame, "SmartSchool Guard", (10, 30),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
+
+    cv2.imshow("Camera Celular", frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+camera.release()
+cv2.destroyAllWindows()
